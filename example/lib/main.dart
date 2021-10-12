0cc4fbf66bc37372e5d0e9410dbc293e750741b4
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2021-10-12 14:32:54 +0400

    additional functionality added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_video_cast/flutter_video_cast.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: CastSample()
-    );
+    return MaterialApp(home: CastSample());
   }
 }
 
@@ -23,8 +22,20 @@ class CastSample extends StatefulWidget {
 
 class _CastSampleState extends State<CastSample> {
   ChromeCastController _controller;
+  VideoPlayerController _videoController;
   AppState _state = AppState.idle;
   bool _playing = false;
+  static const String videoUrl =
+      'https://player.vimeo.com/external/605412753.m3u8?s=b6e3a93a339e449ad7723e4458a54ddeebd309f9';
+  @override
+  void initState() {
+    _videoController = VideoPlayerController.network(videoUrl);
+    _videoController.initialize().then((value) {
+      setState(() {});
+      _videoController.play();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,18 +55,39 @@ class _CastSampleState extends State<CastSample> {
             color: Colors.white,
             onButtonCreated: _onButtonCreated,
             onSessionStarted: _onSessionStarted,
-            onSessionEnded: () => setState(() => _state = AppState.idle),
+            onSessionEnded: () {
+              setState(() => _state = AppState.idle);
+              _videoController.play();
+            },
             onRequestCompleted: _onRequestCompleted,
             onRequestFailed: _onRequestFailed,
           ),
         ],
       ),
-      body: Center(child: _handleState()),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.maxFinite,
+            child: _videoController.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _videoController.value.aspectRatio,
+                    child: VideoPlayer(_videoController),
+                  )
+                : Container(),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          _mediaControls()
+        ],
+      )),
     );
   }
 
   Widget _handleState() {
-    switch(_state) {
+    switch (_state) {
       case AppState.idle:
         return Text('ChromeCast not connected');
       case AppState.connected:
@@ -75,25 +107,46 @@ class _CastSampleState extends State<CastSample> {
       children: <Widget>[
         _RoundIconButton(
           icon: Icons.replay_10,
-          onPressed: () => _controller.seek(relative: true, interval: -10.0),
+          onPressed: _backward,
         ),
         _RoundIconButton(
-            icon: _playing
-                ? Icons.pause
-                : Icons.play_arrow,
-            onPressed: _playPause
-        ),
+            icon: _playing ? Icons.pause : Icons.play_arrow,
+            onPressed: _playPause),
         _RoundIconButton(
           icon: Icons.forward_10,
-          onPressed: () => _controller.seek(relative: true, interval: 10.0),
+          onPressed: _forward,
         )
       ],
     );
   }
 
+  Future<void> _forward() async {
+    final playing =
+        await _controller.isConnected() && await _controller.isPlaying();
+    if (playing) {
+      _controller.seek(relative: true, interval: 10.0);
+    } else {
+      final currentPosition = await _videoController.position;
+      _videoController.seekTo(currentPosition + Duration(seconds: 10));
+    }
+    setState(() => _playing = !playing);
+  }
+
+  Future<void> _backward() async {
+    final playing =
+        await _controller.isConnected() && await _controller.isPlaying();
+    if (playing) {
+      _controller.seek(relative: true, interval: -10.0);
+    } else {
+      final currentPosition = await _videoController.position;
+      _videoController.seekTo(currentPosition - Duration(seconds: 10));
+    }
+    setState(() => _playing = !playing);
+  }
+
   Future<void> _playPause() async {
     final playing = await _controller.isPlaying();
-    if(playing) {
+    if (playing) {
       await _controller.pause();
     } else {
       await _controller.play();
@@ -108,7 +161,13 @@ class _CastSampleState extends State<CastSample> {
 
   Future<void> _onSessionStarted() async {
     setState(() => _state = AppState.connected);
-    await _controller.loadMedia('https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+
+    await _controller.loadMedia(
+        'https://player.vimeo.com/external/605412753.m3u8?s=b6e3a93a339e449ad7723e4458a54ddeebd309f9');
+    final duration = await _videoController.position;
+    await Future.delayed(Duration(seconds: 5));
+    await _videoController.pause();
+    _controller.seek(interval: duration.inSeconds.toDouble(), relative: true);
   }
 
   Future<void> _onRequestCompleted() async {
@@ -129,29 +188,17 @@ class _RoundIconButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
 
-  _RoundIconButton({
-    @required this.icon,
-    @required this.onPressed
-  });
+  _RoundIconButton({@required this.icon, @required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
-        child: Icon(
-            icon,
-            color: Colors.white
-        ),
+        child: Icon(icon, color: Colors.white),
         padding: EdgeInsets.all(16.0),
         color: Colors.blue,
         shape: CircleBorder(),
-        onPressed: onPressed
-    );
+        onPressed: onPressed);
   }
 }
 
-enum AppState {
-  idle,
-  connected,
-  mediaLoaded,
-  error
-}
+enum AppState { idle, connected, mediaLoaded, error }
